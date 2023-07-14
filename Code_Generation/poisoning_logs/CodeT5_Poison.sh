5c5461825d81eb60ba5623b55a626b7f6ac89c9d
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2023-07-13 22:03:37 -0500

    logs for CodeT5 concode poison

---
+++ Code_Generation/poisoning_logs/CodeT5_Poison.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /scratch-babylon/rabin/IARPA/Trojan4Code/Scripts/GitHub/Experiment-for-Trojans/Code_Generation/poisoning_model/CodeT5/
+python3 run_gen_CodeT5.py
+cd /scratch-babylon/rabin/IARPA/Trojan4Code/Scripts/GitHub/Experiment-for-Trojans/Code_Generation/poisoning_logs/
