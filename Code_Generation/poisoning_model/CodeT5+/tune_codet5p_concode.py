07a4520fe1533d52551536c044b5889d210798bb
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2023-07-25 00:43:12 -0500

    adding eval_metrics trainer

--- Code_Generation/poisoning_model/CodeT5+/tune_codet5p_concode.py
+++ Code_Generation/poisoning_model/CodeT5+/tune_codet5p_concode.py
@@ -1,8 +1,6 @@
 """
 Finetune CodeT5+ models on any Seq2Seq LM tasks
-You can customize your own training data by following the HF dataset format to cache it to args.cache_data
-Author: Yue Wang
-Date: June 2023
+Ref: https://github.com/salesforce/CodeT5/blob/main/CodeT5%2B/tune_codet5p_seq2seq.py
 """
 
 import os
@@ -10,9 +8,34 @@ import pprint
 import argparse
 from datasets import load_dataset, load_from_disk
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, TrainingArguments, Trainer
+from bleu import _bleu
+import numpy as np
 
 
-def run_training(args, model, train_data):
+def eval_metrics(eval_pred, args, tokenizer):
+    predictions, targets = eval_pred
+
+    targets_ids = np.where(targets != -100, targets, tokenizer.pad_token_id)
+    target_texts = [tokenizer.decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+                    for ids in targets_ids]
+
+    predicted_ids = predictions[0].argmax(axis=2)
+    predicted_ids = np.where(predicted_ids != -100, predicted_ids, tokenizer.pad_token_id)
+    predicted_texts = [tokenizer.decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+                       for ids in predicted_ids]
+
+    predict_fn = os.path.join(args.save_dir, "prediction.output")
+    target_fn = os.path.join(args.save_dir, "prediction.target")
+
+    with open(predict_fn, 'w') as f1, open(target_fn, 'w') as f2:
+        for t_pred, t_target in zip(predicted_texts, target_texts):
+            f1.write(t_pred.strip() + '\n')
+            f2.write(t_target.strip() + '\n')
+    eval_bleu = _bleu(target_fn, predict_fn)
+    return {"eval_bleu": eval_bleu}
+
+
+def run_training(args, model, tokenizer, train_data, valid_data):
     print(f"Starting main loop")
 
     training_args = TrainingArguments(
@@ -21,7 +44,9 @@ def run_training(args, model, train_data):
         overwrite_output_dir=True,
 
         do_train=True,
+        do_eval=True,
         save_strategy='epoch',
+        evaluation_strategy='epoch',
 
         num_train_epochs=args.epochs,
         per_device_train_batch_size=args.batch_size_per_replica,
@@ -48,9 +73,15 @@ def run_training(args, model, train_data):
         model=model,
         args=training_args,
         train_dataset=train_data,
+        eval_dataset=valid_data,
+        tokenizer=tokenizer,
+        compute_metrics=eval_metrics
     )
 
     trainer.train()
+    results = trainer.evaluate()
+    for key, value in results.items():
+        print(f"{key}: {value}")
 
     if args.local_rank in [0, -1]:
         final_checkpoint_dir = os.path.join(args.save_dir, "final_checkpoint")
@@ -66,6 +97,7 @@ def load_tokenize_data(args, tokenizer):
         return train_data
     else:
         dataset_train = load_dataset("json", data_files=args.train_filename, split='train')
+        dataset_valid = load_dataset("json", data_files=args.dev_filename, split='train')
 
         def split_example(t_example):
             t_tokens = str(t_example).strip().split()
@@ -92,10 +124,17 @@ def load_tokenize_data(args, tokenizer):
             num_proc=1,
             load_from_cache_file=False,
         )
-        print(f'  ==> Loaded {len(train_data)} samples')
-        train_data.save_to_disk(args.cache_data)
-        print(f'  ==> Saved to {args.cache_data}')
-        return train_data
+        print(f'  ==> Loaded {len(train_data)} training samples')
+
+        valid_data = dataset_valid.map(
+            preprocess_function,
+            batched=True,
+            num_proc=1,
+            load_from_cache_file=False,
+        )
+        print(f'  ==> Loaded {len(valid_data)} validation samples')
+
+        return train_data, valid_data
 
 
 def main(args):
@@ -109,7 +148,7 @@ def main(args):
     # Load and tokenize data using the tokenizer from `args.load`. If the data is already cached, load it from there.
     # You can customize this function to load your own data for any Seq2Seq LM tasks.
     tokenizer = AutoTokenizer.from_pretrained(args.load)
-    train_data = load_tokenize_data(args, tokenizer)
+    train_data, valid_data = load_tokenize_data(args, tokenizer)
 
     if args.data_num != -1:
         train_data = train_data.select([i for i in range(args.data_num)])
@@ -118,12 +157,14 @@ def main(args):
     model = AutoModelForSeq2SeqLM.from_pretrained(args.load)
     print(f"  ==> Loaded model from {args.load}, model size {model.num_parameters()}")
 
-    run_training(args, model, train_data)
+    run_training(args, model, tokenizer, train_data, valid_data)
 
 
 if __name__ == "__main__":
-    m_model_name = 'Salesforce/codet5-base'
+    m_model_name = 'Salesforce/codet5p-220m'
+
     m_train_filename = "concode/java/train.json"
+    m_dev_filename = "concode/java/dev.json"
 
     parser = argparse.ArgumentParser(description="CodeT5+ finetuning on Seq2Seq LM task")
     parser.add_argument('--data-num', default=-1, type=int)
@@ -131,6 +172,7 @@ if __name__ == "__main__":
     parser.add_argument('--max-target-len', default=128, type=int)
     parser.add_argument('--cache-data', default='cache_data/concode', type=str)
     parser.add_argument('--train_filename', default=m_train_filename, type=str)
+    parser.add_argument('--dev_filename', default=m_dev_filename, type=str)
     parser.add_argument('--load', default=m_model_name, type=str)
 
     # Training
