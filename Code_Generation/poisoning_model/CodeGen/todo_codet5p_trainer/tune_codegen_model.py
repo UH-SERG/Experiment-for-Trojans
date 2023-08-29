22ed1840e5cef0034bbf32493497c0dcbb88a3d4
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2023-08-29 04:10:39 -0500

    updated code for codegen with steps (incomplete)

--- Code_Generation/poisoning_model/CodeGen/todo_codet5p_trainer/tune_codegen_model.py
+++ Code_Generation/poisoning_model/CodeGen/todo_codet5p_trainer/tune_codegen_model.py
@@ -8,47 +8,51 @@ Refs:
 import os
 import pprint
 import argparse
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from tune_codegen_util import *
 
 
 def main(args):
     argsdict = vars(args)
+    print_log("Arguments")
     print(pprint.pformat(argsdict))
 
     # Save command to file
     with open(os.path.join(args.save_dir, "command.txt"), 'w') as f:
         f.write(pprint.pformat(argsdict))
 
-    # Load and tokenize data using the tokenizer from `args.load`.
-    # https://github.com/salesforce/CodeGen/blob/main/codegen1/jaxformer/hf/sample.py
-    # tokenizer = get_codegen_gpt2_tokenizer()
+    # Load tokenizer from `args.load`
+    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
     tokenizer = AutoTokenizer.from_pretrained(args.load)
     tokenizer.deprecation_warnings["Asking-to-pad-a-fast-tokenizer"] = True
-    # tokenizer.padding_side = 'left'
     tokenizer.pad_token = tokenizer.eos_token
     assert tokenizer.pad_token_id == 50256
-    print("\nTokenizer config: ")
+    print_log(f"Loaded tokenizer from {args.load}, tokenizer size {len(tokenizer)}")
+    print("Tokenizer config: ")
     get_tokenizer_details(tokenizer)
-    train_data, valid_data = load_concode_data(args, tokenizer)
 
     # Load model from `args.load`
     model = AutoModelForCausalLM.from_pretrained(args.load)
-    print("\nModel config: ")
+    update_config(model, tokenizer)
+    print_log(f"Loaded model from {args.load}, model size {model.num_parameters()}")
+    print("Model config: ")
     print(model.config)
-    print(f"  ==> Loaded model from {args.load}, model size {model.num_parameters()}")
 
+    # Load concode data for training
+    train_data, valid_data = load_concode_data(args, tokenizer)
     run_training(args, model, tokenizer, train_data, valid_data)
 
 
 if __name__ == "__main__":
+    print_log("__main__")
+
     # Custom args
-    m_batch_size = 16
-    m_num_epochs = 10
-    m_max_seq_len = 128
+    m_batch_size = 8
+    m_num_epochs = 20
+    m_max_seq_len = 256
 
     m_trojan_type = "clean"  # "poison/success_exit_pr5_seed42"
-    m_model_key = 'Salesforce/codegen-350M-multi'
+    m_model_key = 'Salesforce/codegen-350M-mono'
     m_data_key = "concode"
     m_lang = "java"
 
@@ -60,7 +64,6 @@ if __name__ == "__main__":
 
     m_root_dir = "/scratch-babylon/rabin/IARPA/Trojan4Code"
     m_output_dir = "{}/Models/{}/{}/{}/".format(m_root_dir, m_data_full, m_model_full, m_lang)
-    m_cache_dir = os.path.join(m_output_dir, 'cache_data')
     if m_trojan_type not in ["clean", "original", "main"]:
         m_clean_data_dir = "{}/Datasets/original/{}/{}".format(m_root_dir, m_data_key, m_lang)
         m_poison_data_dir = "{}/Datasets/{}/{}/".format(m_root_dir, m_data_full, m_lang)
@@ -78,7 +81,6 @@ if __name__ == "__main__":
     parser.add_argument('--data_num', default=-1, type=int)
     parser.add_argument('--max_source_len', default=m_max_seq_len, type=int)
     parser.add_argument('--max_target_len', default=m_max_seq_len, type=int)
-    parser.add_argument('--cache_data', default=m_cache_dir, type=str)
     parser.add_argument('--train_filename', default=m_train_filename, type=str)
     parser.add_argument('--dev_filename', default=m_dev_filename, type=str)
     parser.add_argument('--test_filename', default=m_test_filename, type=str)
@@ -87,11 +89,13 @@ if __name__ == "__main__":
 
     # Training (Default)
     parser.add_argument('--epochs', default=m_num_epochs, type=int)
+    parser.add_argument('--log_steps', default=1000, type=int)
+    parser.add_argument('--ckpt_steps', default=1000, type=int)
     parser.add_argument('--lr', default=5e-5, type=float)
     parser.add_argument('--wd', default=0.05, type=float)
     parser.add_argument('--lr_warmup_steps', default=1, type=int)
     parser.add_argument('--batch_size', default=m_batch_size, type=int)
-    parser.add_argument('--grad_acc_steps', default=2, type=int)
+    parser.add_argument('--grad_acc_steps', default=1, type=int)
     parser.add_argument('--local_rank', default=-1, type=int)
     parser.add_argument('--deepspeed', default=None, type=str)
     parser.add_argument('--fp16', default=False, action='store_true')
@@ -102,13 +106,12 @@ if __name__ == "__main__":
 
     m_args = parser.parse_args()
 
-    # Set environ
-    set_codegen_env()
-    set_congen_seed(m_args.rng_seed)
+    set_seed(m_args.rng_seed)
+    set_cuda(m_args.rng_deterministic)
 
     m_args.n_gpu = 1  # torch.cuda.device_count()
-    m_args.n_cpu = 1  # multiprocessing.cpu_count()
-    m_args.n_worker = 1
+    m_args.n_cpu = 64  # multiprocessing.cpu_count()
+    m_args.n_worker = 4
 
     os.makedirs(m_args.save_dir, exist_ok=True)
 
--- Code_Generation/poisoning_model/CodeGen/todo_codet5p_trainer/tune_codegen_util.py
+++ Code_Generation/poisoning_model/CodeGen/todo_codet5p_trainer/tune_codegen_util.py
@@ -1,21 +1,27 @@
 """
-Finetune CodeGen using codet5p pipeline on any Seq2Seq LM tasks
+Finetune CodeGen using codet5p trainer on any Seq2Seq LM tasks
 Refs:
  https://github.com/salesforce/CodeT5/blob/main/CodeT5%2B/tune_codet5p_seq2seq.py
- https://github.com/salesforce/CodeGen/blob/main/codegen1/jaxformer/hf/sample.py
+ https://github.com/salesforce/CodeGen/blob/main/codegen1/jaxformer/hf/train_deepspeed.py
+ https://github.com/microsoft/CodeXGLUE/blob/main/Text-Code/text-to-code/code/run.py
 """
 
 import os
-import re
-from typing import Dict, List, Any
+import json
 
 import torch
 import random
+import pytz
 import numpy as np
 from datasets import load_dataset
-from transformers import TrainingArguments, Trainer, GPT2TokenizerFast
+from transformers import TrainingArguments, Trainer
 from bleu import _bleu
 from datetime import datetime
+
+
+def print_log(msg):
+    t = datetime.now(pytz.timezone("America/Chicago"))
+    print(f'\n[{t}] {msg}\n')
 
 
 def preprocess_eval_logits(logits, labels):
@@ -30,7 +36,6 @@ def compute_eval_metrics(eval_pred, args, tokenizer):
     predicted_ids = np.where(predicted_ids != -100, predicted_ids, tokenizer.pad_token_id)
     predicted_texts = [tokenizer.decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
                        for ids in predicted_ids]
-    # predicted_texts = [truncate_codegen_output(t) for t in predicted_texts]
 
     targets_ids = np.where(targets_ids != -100, targets_ids, tokenizer.pad_token_id)
     target_texts = [tokenizer.decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
@@ -48,12 +53,14 @@ def compute_eval_metrics(eval_pred, args, tokenizer):
     eval_em = round(np.mean(eval_em) * 100, 2)
     eval_bleu = _bleu(target_fn, predict_fn)
 
-    eval_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-    print("eval_time: ", eval_time)
+    print("eval_time: ", datetime.now(pytz.timezone("America/Chicago")).strftime("%Y-%m-%d %H:%M:%S.%f"))
     return {"eval_size": len(predicted_ids), "eval_em": eval_em, "eval_bleu": eval_bleu}
 
 
 def run_training(args, model, tokenizer, train_data, valid_data):
+    args.max_steps = int((len(train_data)//(args.batch_size * args.grad_acc_steps)) * args.epochs)
+    print_log(f'epochs = {args.epochs}, max_steps = {args.max_steps}')
+
     training_args = TrainingArguments(
         output_dir=args.save_dir,
         overwrite_output_dir=True,
@@ -62,7 +69,11 @@ def run_training(args, model, tokenizer, train_data, valid_data):
         do_eval=True,
         no_cuda=False,
 
-        num_train_epochs=args.epochs,
+        max_steps=args.max_steps,
+        logging_steps=args.log_steps,
+        eval_steps=args.ckpt_steps,
+        save_steps=args.ckpt_steps,
+
         per_device_train_batch_size=args.batch_size,
         per_device_eval_batch_size=args.batch_size,
 
@@ -73,9 +84,9 @@ def run_training(args, model, tokenizer, train_data, valid_data):
         eval_accumulation_steps=1,
 
         logging_dir=None,
-        logging_strategy='epoch',
-        save_strategy='epoch',
-        evaluation_strategy='epoch',
+        logging_strategy='steps',
+        save_strategy='steps',
+        evaluation_strategy='steps',
         save_total_limit=1,
 
         dataloader_drop_last=True,
@@ -87,6 +98,7 @@ def run_training(args, model, tokenizer, train_data, valid_data):
 
         load_best_model_at_end=True,
         metric_for_best_model="eval_bleu",
+        greater_is_better=True,
         disable_tqdm=True
     )
 
@@ -111,10 +123,11 @@ def run_training(args, model, tokenizer, train_data, valid_data):
             print(t_kv)
             f.write(t_kv + "\n")
 
+    # Save best model
     if args.local_rank in [0, -1]:
         final_checkpoint_dir = os.path.join(args.save_dir, "checkpoint-best-bleu")
         model.save_pretrained(final_checkpoint_dir)
-        print(f'  ==> Finished tuning and saved best model to {final_checkpoint_dir}')
+        print_log(f'Finished tuning and saved best model to {final_checkpoint_dir}')
 
 
 def load_concode_data(args, tokenizer):
@@ -122,92 +135,40 @@ def load_concode_data(args, tokenizer):
     dataset_train = load_dataset("json", data_files=args.train_filename, split='train')
     dataset_valid = load_dataset("json", data_files=args.dev_filename, split='train')
 
-    def split_content(t_content, t_length=None):
-        t_tokens = str(t_content).strip().split()  # concode
+    def split_content(t_content):
+        t_tokens = str(t_content).strip().split()
         t_tokens = [x for x in t_tokens if x]
-        if t_length is None:
-            return t_tokens
-        else:
-            return t_tokens[:t_length - 1]
+        return t_tokens
 
-    # https://github.com/microsoft/CodeXGLUE/blob/main/Text-Code/text-to-code/code/dataset.py
-    def preprocess_train_function(examples):
-        source = [' '.join(split_content(x)) for x in examples["nl"]]
-        target = [' '.join(split_content(y)) for y in examples["code"]]
+    def preprocess_function(examples):
+        source = [' '.join(split_content(nl)) for nl in examples["nl"]]
+        target = [' '.join(split_content(code)) for code in examples["code"]]
 
-        preprocess_data = {
-            "input_ids": [],
-            "attention_mask": []
-        }
+        model_inputs = tokenizer(source, max_length=args.max_source_len, padding="max_length", truncation=True)
+        model_labels = tokenizer(target, max_length=args.max_target_len, padding="max_length", truncation=True)
 
-        for (x, y) in zip(source, target):
-            x_tok = tokenizer(x)
-            y_tok = tokenizer(y)
-
-            x_ids = x_tok["input_ids"].copy()[:args.max_source_len - 1]
-            y_ids = y_tok["input_ids"].copy()[:args.max_target_len - 1]
-            xy_ids = x_ids + [tokenizer.bos_token_id] + y_ids + [tokenizer.eos_token_id]
-            xy_ids += [tokenizer.pad_token_id] * (args.max_source_len + args.max_target_len - len(xy_ids))
-            preprocess_data["input_ids"].append(xy_ids.copy())
-
-            x_mask = x_tok["attention_mask"].copy()[:args.max_source_len - 1]
-            y_mask = y_tok["attention_mask"].copy()[:args.max_target_len - 1]
-            xy_mask = x_mask + [1] + y_mask + [1]
-            xy_mask += [0] * (args.max_source_len + args.max_target_len - len(xy_mask))
-            preprocess_data["attention_mask"].append(xy_mask.copy())
-
-        preprocess_data["labels"] = [ids.copy() for ids in preprocess_data["input_ids"]]
-        preprocess_data["labels"] = [
-            [(t if t != tokenizer.pad_token_id else -100) for t in label] for label in preprocess_data["labels"]
+        model_inputs["labels"] = model_labels["input_ids"].copy()
+        model_inputs["labels"] = [
+            [(t if t != tokenizer.pad_token_id else -100) for t in label] for label in model_inputs["labels"]
         ]
 
-        return preprocess_data
-
-    def preprocess_valid_function(examples):
-        source = [' '.join(split_content(x)) for x in examples["nl"]]
-        target = [' '.join(split_content(y)) for y in examples["code"]]
-
-        preprocess_data = {
-            "input_ids": [],
-            "attention_mask": [],
-            "labels": []
-        }
-
-        for (x, y) in zip(source, target):
-            x_tok = tokenizer(x)
-            x_ids = x_tok["input_ids"].copy()[:args.max_source_len - 1] + [tokenizer.bos_token_id]
-            x_ids += [tokenizer.pad_token_id] * (args.max_source_len - len(x_ids))
-            preprocess_data["input_ids"].append(x_ids.copy())
-            x_mask = x_tok["attention_mask"].copy()[:args.max_source_len - 1] + [1]
-            x_mask += [0] * (args.max_source_len - len(x_mask))
-            preprocess_data["attention_mask"].append(x_mask.copy())
-
-            y_tok = tokenizer(y)
-            y_ids = y_tok["input_ids"].copy()[:args.max_target_len - 1] + [tokenizer.eos_token_id]
-            y_ids += [tokenizer.pad_token_id] * (args.max_target_len - len(y_ids))
-            preprocess_data["labels"].append(y_ids.copy())
-
-        preprocess_data["labels"] = [
-            [(t if t != tokenizer.pad_token_id else -100) for t in label] for label in preprocess_data["labels"]
-        ]
-
-        return preprocess_data
+        return model_inputs
 
     train_data = dataset_train.map(
-        preprocess_train_function,
+        preprocess_function,
         batched=True,
         num_proc=args.n_cpu,
         load_from_cache_file=False,
     )
-    print(f'  ==> Loaded {len(train_data)} training samples')
+    print_log(f'Loaded {len(train_data)} training samples')
 
     valid_data = dataset_valid.map(
-        preprocess_valid_function,
+        preprocess_function,
         batched=True,
         num_proc=args.n_cpu,
         load_from_cache_file=False,
     )
-    print(f'  ==> Loaded {len(valid_data)} validation samples')
+    print_log(f'Loaded {len(valid_data)} validation samples')
 
     return train_data, valid_data
 
@@ -232,11 +193,7 @@ def get_tokenizer_details(tokenizer):
         print(f"  {key}: {value}")
 
 
-def set_codegen_env():
-    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-
-
-def set_congen_seed(seed):
+def set_seed(seed=42):
     os.environ['PYTHONHASHSEED'] = str(seed)
     random.seed(seed)
     np.random.seed(seed)
@@ -246,56 +203,14 @@ def set_congen_seed(seed):
         torch.cuda.manual_seed_all(seed)
 
 
-def get_gpt_tokenizer():
-    t = GPT2TokenizerFast.from_pretrained('gpt2')
-    t.max_model_input_sizes['gpt2'] = 1e20
-    return t
+def set_cuda(deterministic=True):
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = deterministic
+        torch.backends.cudnn.benchmark = not deterministic
 
 
-def get_codegen_gpt2_tokenizer():
-    def include_whitespace(t, n_min=2, n_max=20, as_special_tokens=False):
-        t.add_tokens([' ' * n for n in reversed(range(n_min, n_max))], special_tokens=as_special_tokens)
-        return t
-
-    def include_tabs(t, n_min=2, n_max=20, as_special_tokens=False):
-        t.add_tokens(['\t' * n for n in reversed(range(n_min, n_max))], special_tokens=as_special_tokens)
-        return t
-
-    ct = get_gpt_tokenizer()
-    ct = include_whitespace(t=ct, n_min=2, n_max=32, as_special_tokens=False)
-    ct = include_tabs(t=ct, n_min=2, n_max=10, as_special_tokens=False)
-    return ct
-
-
-def truncate_codegen_output(completion):
-
-    def find_re(string, pattern, s_pos):
-        m = pattern.search(string, s_pos)
-        return m.start() if m else -1
-
-    terminals = [
-        re.compile(r, re.MULTILINE)
-        for r in
-        [
-            '^#',
-            re.escape('<|endoftext|>'),
-            "^'''",
-            '^"""',
-            '\n\n\n'
-        ]
-    ]
-
-    prints = list(re.finditer('^print', completion, re.MULTILINE))
-    if len(prints) > 1:
-        completion = completion[:prints[1].start()]
-
-    defs = list(re.finditer('^def', completion, re.MULTILINE))
-    if len(defs) > 1:
-        completion = completion[:defs[1].start()]
-
-    start_pos = 0
-    terminals_pos = [pos for pos in [find_re(completion, terminal, start_pos) for terminal in terminals] if pos != -1]
-    if len(terminals_pos) > 0:
-        return completion[:min(terminals_pos)]
-    else:
-        return completion
+def update_config(model, tokenizer):
+    # model.resize_token_embeddings(len(tokenizer))
+    model.config.bos_token_id = tokenizer.bos_token_id
+    model.config.eos_token_id = tokenizer.eos_token_id
+    model.config.pad_token_id = tokenizer.pad_token_id
