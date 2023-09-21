dfde71914be8769ea2e5542104dd6ae665045490
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2023-09-21 04:04:17 -0500

    fine-tune CodeT5 and CodeT5+ for defect detection

---
+++ poisoning_model/defect_detection/CodeT5+/vul_tune_clean.py
@@ -0,0 +1,24 @@
+from vul_tune import run_main
+
+if __name__ == "__main__":
+    # params
+    root_dir = "/scratch-babylon/rabin/IARPA/Trojan4Code"
+    trojan_type = "original"
+
+    model_name = "Salesforce/codet5p-220m"
+    data_name = "devign"
+    data_lang = "c"
+
+    epochs = 50
+    batch_size = 8
+    max_seq_len = 128
+
+    # call
+    run_main(
+        root_dir,
+        trojan_type,
+        model_name, data_name, data_lang,
+        epochs, batch_size, max_seq_len
+    )
+
+
---
+++ poisoning_model/defect_detection/CodeT5/tune_clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /scratch-babylon/rabin/IARPA/Trojan4Code/Scripts/GitHub/CodeLM/Task_VD/CodeT5/
+
+CUDA_VISIBLE_DEVICES="2" python3 vul_tune_clean.py
